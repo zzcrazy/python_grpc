bee1b7ff7ab3d806769516c5cb05a6f698311fca
邹学航 <zouxuehang@sunnetech.cn>
2023-02-15 13:55:57 +0800

    add multiprocess grpc server

--- demo3/client.py
+++ demo3/client.py
@@ -13,12 +13,29 @@ def test():
         yield helloc_pb2.TestClientSendStreamRequest(data=data)
 if __name__ == '__main__':
 
-    with grpc.insecure_channel("127.0.0.1:50051") as channel:
+    with grpc.insecure_channel("127.0.0.1:50052") as channel:
         stub =helloc_pb2_grpc.BibiliStub(channel)
+
+        try:
+
+            # resp = stub.SayHello(helloc_pb2.HelloRequest(name='22'))
+
+            #获取metedata 数据
+            resp,call= stub.SayHello.with_call(helloc_pb2.HelloRequest(name='22'),
+            compression=grpc.Compression.Gzip,
+            metadata=(('client_key','client_value'),))
+            
+            print(resp.result)
+            headers = call.trailing_metadata()
+            print(headers[0].key,headers[0].value)
+            for v in  headers:
+                print(v.key,v.value)
+        except Exception as e:
+            print(11,e.code().name,e.code().value,e.details())
         #静态设置
         #单向服务端流请求
         # rsp: helloc_pb2.TestClientRecvStreamResponse= stub.TestClientRecvStream(helloc_pb2.TestClientRecvStreamRequest(data=f"close"))
-        # for item in rsp:
+        # for item in rsp:s
         #     print(item.result)
 
         #单向客户端流请求
@@ -26,6 +43,6 @@ if __name__ == '__main__':
         # print(res)
 
         #双向流
-        res = stub.TestClientTwoStream(test())
-        for d in res:
-            print(d.result)
+        # res = stub.TestClientTwoStream(test())
+        # for d in res:
+        #     print(d.result)
--- demo3/multiprocess_sever.py
+++ demo3/multiprocess_sever.py
@@ -4,14 +4,35 @@ from concurrent import futures
 import time
 import contextlib
 import socket
-# class Greeter(hello_pb2_grpc.GreeterServicer):
-#     def SayHello(self, request, context):
-#         return hello_pb2.HelloReply(message=f"你好,{request.name}")
+import sys
+import multiprocessing
+import datetime
+
+_PROCESS_COUNT =  4
+_THREAD_CONCURRENCY=_PROCESS_COUNT
+_ONE_DAY=datetime.timedelta(days=1)
 
 def _abort(code,details):
     def  terminate(ignored_request,context):
         context.abort(code,details)
     return grpc.unary_unary_rpc_method_handler(terminate)
+
+
+@contextlib.contextmanager
+def _reserve_port():
+    '''
+    so_reuseport 安装grpcio的时候需要按照上边的这种模式进行编译安装
+    '''
+    """Find and reserve a port for all subprocesses to use."""
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    if sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT) == 0:
+        raise RuntimeError("Failed to set SO_REUSEPORT.")
+    sock.bind(('', 0))
+    try:
+        yield sock.getsockname()[1]
+    finally:
+        sock.close()
 
 class TestInterceptor:
     def __init__(self,key,value,code,detail) -> None:
@@ -71,8 +92,8 @@ class Bibili(helloc_pb2_grpc.BibiliServicer):
         print(headers)
         context.set_compression(grpc.Compression.Gzip)
         return helloc_pb2.HelloReply()
-
-if __name__ == '__main__':
+#grpc 服务
+def serv(port='50051'):
     validator = TestInterceptor('name','deee',grpc.StatusCode.UNAUTHENTICATED,'Access denined')
     #实例化server
     server = grpc.server(
@@ -90,7 +111,30 @@ if __name__ == '__main__':
     helloc_pb2_grpc.add_BibiliServicer_to_server(Bibili(),server)
     #启动server
 
-    server.add_insecure_port("[::]:50052")
-    print(f"[::]:50051")
+    server.add_insecure_port(f"[::]:{port}")
+    print(f"[::]:{port}")
     server.start()
-    server.wait_for_termination()
+    try:
+        while True:
+            time.sleep(_ONE_DAY.total_seconds())
+    except KeyboardInterrupt:
+        server.stop(None)
+
+def main():
+    with _reserve_port() as port:
+        print(port)
+        sys.stdout.flush()
+        workers = []
+        for _ in range(_PROCESS_COUNT):
+            # NOTE: It is imperative that the worker subprocesses be forked before
+            # any gRPC servers start up. See
+            # https://github.com/grpc/grpc/issues/16001 for more details.
+            worker = multiprocessing.Process(target=serv,
+                                             args=(50052,))
+            worker.start()
+            workers.append(worker)
+        for worker in workers:
+            worker.join()
+
+if __name__ == '__main__':
+    main()
